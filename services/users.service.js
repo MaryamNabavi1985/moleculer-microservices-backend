8f330ff270b1268ca5f8fc1e295af814a547c356
maryam.nabavi <maryam.s.nabavi@gmail.com>
2026-02-03 22:20:32 +0330

    feat: add params validation to user service actions

--- services/users.service.js
+++ services/users.service.js
@@ -2,26 +2,41 @@
 
 module.exports = {
     name: "users",
+
     actions : {
-        list(){
-            return [
+        list:{
+            handler(){
+                return [
                 {id:1, name:"Maryam"},
                 {id:2, name:"Morteza"},
                 {id:3, name:"Ardavan"}
             ]
+            }
+          
         },
 
-        get(ctx){
-            const id =ctx.params.id;
-            return {id: id, name:"Maryam"};
+             get: {
+            params: {
+                id: "number|convert"
+            },
+            handler(ctx) {
+                return {
+                    id: ctx.params.id,
+                    name: "Maryam"
+                };
+            }
         },
-
-        create(ctx){
-            const user= ctx.params;
-            return {
+        create:{
+            params: {
+                name: "string|min:3"
+            },  
+            handler(ctx){
+                 return {
                 id: Date.now(),
-                ...user
+                name: ctx.params.name
             };
+            }  
+           
         }
     }
 };
